b99875301c3b53333fbf95994a6e1f744e4372fe
Laura Rapp <rapp.laurad@gmail.com>
2022-02-06 10:43:10 -0500

    added querySelector() on first h2 and changed text content

--- Unit_3/exercise_3/exercise_3/form.js
+++ Unit_3/exercise_3/exercise_3/form.js
@@ -2,6 +2,6 @@
 
 window.addEventListener('load', (event) =>{
 		
-    
-    
+    const h2TextPhrase = document.querySelector('h2');
+    h2TextPhrase.textContent = 'Video Game Cost Calculator'
      });
