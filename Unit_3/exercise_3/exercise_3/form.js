bc934af3b80a6508e324d2de1a445aafe840df4c
Laura Rapp <rapp.laurad@gmail.com>
2022-02-06 14:05:13 -0500

    added prompt which asks user price of videogame and targets id values to calculate sales tax and total cost

--- Unit_3/exercise_3/exercise_3/form.js
+++ Unit_3/exercise_3/exercise_3/form.js
@@ -5,8 +5,15 @@ window.addEventListener('load', (event) =>{
     const h2TextPhrase = document.querySelector('h2'); //Targets first H2 tag and adds text upon laod
     h2TextPhrase.textContent = 'Video Game Cost Calculator'
 
-    const EmptyPText = document.getElementById('gameList') //Targets and replaces text in P tag upon load
+    const EmptyPText = document.getElementById('gameList'); //Targets and replaces text in P tag upon load
     EmptyPText.textContent = 'To calculate the cost of the game, type the price of the game within the prompt.'
 
     document.getElementById('logo').src = 'img/logo.png'; // Changes img upon load
+
+     let videoGamePrice = prompt('What is the price of the game?');
+     const taxRate = .05;
+     let salesTax = (parseInt(videoGamePrice) * taxRate.toFixed(2));
+
+     document.getElementById('sales').value = '$' + salesTax;
+     document.getElementById('total').value = '$' + (parseInt(videoGamePrice) + salesTax);
      });
