0838f4d8c54e55b8ab1bdfd8eeaafa3d4cc2d3ae
Laura Rapp <rapp.laurad@gmail.com>
2022-02-06 11:04:19 -0500

    Added getElementById() targeting empty p tag and added text upon page load

--- Unit_3/exercise_3/exercise_3/form.js
+++ Unit_3/exercise_3/exercise_3/form.js
@@ -4,4 +4,7 @@ window.addEventListener('load', (event) =>{
 		
     const h2TextPhrase = document.querySelector('h2');
     h2TextPhrase.textContent = 'Video Game Cost Calculator'
+
+    const EmptyPText = document.getElementById('gameList')
+    EmptyPText.textContent = 'To calculate the cost of the game, type the price of the game within the prompt.'
      });
