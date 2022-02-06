d44d9f1b0de806cffd476d8253a38c037bb1a67b
Laura Rapp <rapp.laurad@gmail.com>
2022-02-06 11:27:16 -0500

    Added comments, added getElementById() that targets and changes gaming logo upon load

--- Unit_3/exercise_3/exercise_3/form.js
+++ Unit_3/exercise_3/exercise_3/form.js
@@ -2,9 +2,11 @@
 
 window.addEventListener('load', (event) =>{
 		
-    const h2TextPhrase = document.querySelector('h2');
+    const h2TextPhrase = document.querySelector('h2'); //Targets first H2 tag and adds text upon laod
     h2TextPhrase.textContent = 'Video Game Cost Calculator'
 
-    const EmptyPText = document.getElementById('gameList')
+    const EmptyPText = document.getElementById('gameList') //Targets and replaces text in P tag upon load
     EmptyPText.textContent = 'To calculate the cost of the game, type the price of the game within the prompt.'
+
+    document.getElementById('logo').src = 'img/logo.png'; // Changes img upon load
      });
